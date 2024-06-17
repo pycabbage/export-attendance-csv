dbca518fde65fb84634754a84effa567ba3095a6
Cabbage Lettuce <33714346+pycabbage@users.noreply.github.com>
2024-06-17 17:56:48 +0900

    add husky and lint-staged

--- scripts/build.ts
+++ scripts/build.ts
@@ -18,7 +18,7 @@ await build({
     js: createBanner({
       name: "MF勤怠をCSVにエクスポート",
       description: "MF勤怠をCSVにエクスポート",
-      version: `${packageJson.version}${env.NODE_ENV !== "production" ? `-dev-${new Date().toISOString().replaceAll(/[-:\.TZ]/g, "")}` : ""}`,
+      version: `${packageJson.version}${env.NODE_ENV !== "production" ? `-dev-${new Date().toISOString().replaceAll(/[-:.TZ]/g, "")}` : ""}`,
       match: "https://attendance.moneyforward.com/my_page/attendances",
       updateURL: "https://github.com/pycabbage/export-attendance-csv/releases/latest/download/export-attendance-csv.user.js",
       downloadURL: "https://github.com/pycabbage/export-attendance-csv/releases/latest/download/export-attendance-csv.user.js",
